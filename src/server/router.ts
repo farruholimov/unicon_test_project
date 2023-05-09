48748bf808edcff019c1a135428fa20717529e3f
Farruh Olimov <farruhbekolimov3904@gmail.com>
2023-05-09 20:27:11 +0500

    add orgs route

--- src/server/router.ts
+++ src/server/router.ts
@@ -2,13 +2,16 @@ import { Router } from 'express';
 
 import UsersRoute from '../modules/users/users.route';
 import RolesRoute from '../modules/roles/roles.route';
+import OrgsRoute from '../modules/organizations/organizations.route';
 
 const router = Router();
 
 const usersRoute = new UsersRoute();
 const rolesRoute = new RolesRoute();
+const orgsRoute = new OrgsRoute();
 
 router.use('/', usersRoute.router);
 router.use('/', rolesRoute.router);
+router.use('/', orgsRoute.router);
 
 export default router;
