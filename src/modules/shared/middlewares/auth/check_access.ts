7dad63af19ac35e30ff05b4ebe1260efc27fc7a8
Farruh Olimov <farruhbekolimov3904@gmail.com>
2023-05-09 20:35:20 +0500

    fix wrong import path for role_access service

--- src/modules/shared/middlewares/auth/check_access.ts
+++ src/modules/shared/middlewares/auth/check_access.ts
@@ -1,28 +1,26 @@
-import { RequestWithUser } from "../../interfaces/routes.interface";
-import { NextFunction, Response } from "express";
-import ErrorResponse from "../../utils/errorResponse";
-import RoleAccessService from "src/modules/roles/role_access/role_access.service";
-
+import { RequestWithUser } from '../../interfaces/routes.interface';
+import { NextFunction, Response } from 'express';
+import ErrorResponse from '../../utils/errorResponse';
+import RoleAccessService from '../../../roles/components/role_access/role_access.service';
 
 const check_access = (module: string, level: string) => {
   return async (req: RequestWithUser, res: Response, next: NextFunction) => {
     try {
-      const { user } = req
-      const roleAccessService = new RoleAccessService()
+      const { user } = req;
+      const roleAccessService = new RoleAccessService();
 
       const role_access = await roleAccessService.getByRoleModuleLevel({
         role_id: user.role,
         module,
-        level
-      })
+        level,
+      });
 
-      if (!role_access) throw new ErrorResponse(400, "Access denied!")
-      next()
-
+      if (!role_access) throw new ErrorResponse(400, 'Access denied!');
+      next();
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
-}
+  };
+};
 
 export default check_access;
