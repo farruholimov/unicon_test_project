4723317658e66b71111da3d60902c8ac85a9250c
Farruh Olimov <farruhbekolimov3904@gmail.com>
2023-05-09 16:47:38 +0500

    add users route & roles route

--- src/server/router.ts
+++ src/server/router.ts
@@ -1,5 +1,14 @@
 import { Router } from 'express';
 
+import UsersRoute from '../modules/users/users.route';
+import RolesRoute from '../modules/roles/roles.route';
+
 const router = Router();
+
+const usersRoute = new UsersRoute();
+const rolesRoute = new RolesRoute();
+
+router.use('/', usersRoute.router);
+router.use('/', rolesRoute.router);
 
 export default router;
