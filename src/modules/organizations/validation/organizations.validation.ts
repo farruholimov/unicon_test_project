36e8a63caaba7f41d2dadcb32a45ad944a964e79
Farruh Olimov <farruhbekolimov3904@gmail.com>
2023-05-09 20:24:45 +0500

    create validation for orgs module

---
+++ src/modules/organizations/validation/organizations.validation.ts
@@ -0,0 +1,13 @@
+import { IsDefined, IsNotEmpty, IsString } from 'class-validator';
+import { ICreateOrg, IUpdateOrg } from './organizations.interface';
+
+export class CreateOrgDTO implements ICreateOrg {
+  @IsString()
+  @IsDefined()
+  @IsNotEmpty()
+  name: string;
+}
+export class UpdateOrgDTO implements IUpdateOrg {
+  @IsString()
+  name: string;
+}
