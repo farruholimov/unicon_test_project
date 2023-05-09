7e4e81414b9fa0a6bb00b7922188cd0daa7de1f3
Farruh Olimov <farruhbekolimov3904@gmail.com>
2023-05-09 15:49:27 +0500

    update import route for user interface

--- src/modules/shared/interfaces/routes.interface.ts
+++ src/modules/shared/interfaces/routes.interface.ts
@@ -1,5 +1,5 @@
 import { Router, Request } from 'express';
-import { IUser } from '../../users/interface/users.interface';
+import { IUser } from '../../users/validation/users.interface';
 
 export interface Routes {
   path?: string;
