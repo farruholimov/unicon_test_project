a2e7978fa33fc6fe8df3facf3a58eb7c192124fa
Farruh Olimov <farruhbekolimov3904@gmail.com>
2023-05-10 06:06:53 +0500

    add string array parser util

--- src/modules/shared/utils/utils.ts
+++ src/modules/shared/utils/utils.ts
@@ -1,3 +1,7 @@
 export function getFirst(v: any[]) {
   return v[0];
 }
+
+export function parseStringArray(data: string | unknown[]): any[] {
+  return Array.isArray(data) ? data : typeof data == 'string' ? JSON.parse(data) : [];
+}
