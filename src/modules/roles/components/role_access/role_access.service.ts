8d3181cc00da9a4c2d50ff3d8744f5655794f24d
Farruh Olimov <farruhbekolimov3904@gmail.com>
2023-05-10 06:04:43 +0500

    add existance check

--- src/modules/roles/components/role_access/role_access.service.ts
+++ src/modules/roles/components/role_access/role_access.service.ts
@@ -1,12 +1,10 @@
 import { IRoleAccess, ISelectRoleAccess } from './validation/role_access.interface';
 import RolesDao from './dao/role_access.dao';
-import ErrorResponse from '../../../shared/utils/errorResponse';
 
 export default class RoleAccessService {
   private rolesDao = new RolesDao();
   async getByRoleModuleLevel({ role_id, module, level }: ISelectRoleAccess): Promise<IRoleAccess> {
     const data = await this.rolesDao.selectByRoleModuleLevel({ role_id, module, level });
-    if (!data) throw new ErrorResponse(404, 'Not Found!');
     return data;
   }
 
