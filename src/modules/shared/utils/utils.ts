e1a68f6eef7d1984b2de419d6b9621a095a3a3f4
Farruh Olimov <farruhbekolimov3904@gmail.com>
2023-05-10 17:12:16 +0500

    add update values generator util

--- src/modules/shared/utils/utils.ts
+++ src/modules/shared/utils/utils.ts
@@ -5,3 +5,9 @@ export function getFirst(v: any[]) {
 export function parseStringArray(data: string | unknown[]): any[] {
   return Array.isArray(data) ? data : typeof data == 'string' ? JSON.parse(data) : [];
 }
+
+export function generateUpdateSetClause(body): string {
+  return Object.keys(body)
+    .map((column) => `${column} = ${body[column] ? `'${body[column]}'` : null}`)
+    .join(', ');
+}
