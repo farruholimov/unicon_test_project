dc63c2096bb42ef839aa094084faf19e12992846
Farruh Olimov <farruhbekolimov3904@gmail.com>
2023-05-10 17:00:50 +0500

    add all access modules to admin

--- src/database/seeds/2_role_access.ts
+++ src/database/seeds/2_role_access.ts
@@ -1,38 +1,51 @@
-import { Knex } from "knex";
+import { Knex } from 'knex';
 
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
-  await knex("role_access").del();
+  await knex('role_access').del();
 
   // Inserts seed entries
-  await knex("role_access").insert([
+  await knex('role_access').insert([
     { role_id: 1, module: 'roles', level: 'read' },
-
     { role_id: 1, module: 'users', level: 'create' },
     { role_id: 1, module: 'users', level: 'read' },
     { role_id: 1, module: 'users', level: 'update' },
     { role_id: 1, module: 'users', level: 'delete' },
-
     { role_id: 1, module: 'organizations', level: 'create' },
     { role_id: 1, module: 'organizations', level: 'read' },
     { role_id: 1, module: 'organizations', level: 'update' },
     { role_id: 1, module: 'organizations', level: 'delete' },
-
     { role_id: 1, module: 'organization_users', level: 'create' },
     { role_id: 1, module: 'organization_users', level: 'read' },
     { role_id: 1, module: 'organization_users', level: 'update' },
     { role_id: 1, module: 'organization_users', level: 'delete' },
+    { role_id: 1, module: 'projects', level: 'create' },
+    { role_id: 1, module: 'projects', level: 'read' },
+    { role_id: 1, module: 'projects', level: 'update' },
+    { role_id: 1, module: 'projects', level: 'delete' },
+    { role_id: 1, module: 'tasks', level: 'create' },
+    { role_id: 1, module: 'tasks', level: 'read' },
+    { role_id: 1, module: 'tasks', level: 'update' },
+    { role_id: 1, module: 'tasks', level: 'delete' },
+    { role_id: 1, module: 'stats_all', level: 'read' },
+    { role_id: 1, module: 'stats_org', level: 'read' },
+    { role_id: 1, module: 'stats_project', level: 'read' },
 
+    { role_id: 2, module: 'stats_org', level: 'read' },
+    { role_id: 2, module: 'stats_project', level: 'read' },
+    { role_id: 2, module: 'organization_users', level: 'read' },
+    { role_id: 2, module: 'organizations', level: 'read' },
     { role_id: 2, module: 'projects', level: 'create' },
     { role_id: 2, module: 'projects', level: 'read' },
     { role_id: 2, module: 'projects', level: 'update' },
     { role_id: 2, module: 'projects', level: 'delete' },
-
     { role_id: 2, module: 'tasks', level: 'create' },
     { role_id: 2, module: 'tasks', level: 'read' },
     { role_id: 2, module: 'tasks', level: 'update' },
     { role_id: 2, module: 'tasks', level: 'delete' },
 
+    { role_id: 3, module: 'projects', level: 'read' },
+    { role_id: 3, module: 'organizations', level: 'read' },
     { role_id: 3, module: 'tasks', level: 'read' },
     { role_id: 3, module: 'tasks', level: 'update' },
   ]);
