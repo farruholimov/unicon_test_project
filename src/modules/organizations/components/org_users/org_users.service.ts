383a0983f5b093e22980d1e0dead59c66c075b4f
Farruh Olimov <farruhbekolimov3904@gmail.com>
2023-05-10 05:55:44 +0500

    add create many service & update create one

--- src/modules/organizations/components/org_users/org_users.service.ts
+++ src/modules/organizations/components/org_users/org_users.service.ts
@@ -1,14 +1,33 @@
 import { ICreateOrgUser, IOrgUser } from './validation/org_users.interface';
 import OrgUsersDao from './dao/org_users.dao';
 import ErrorResponse from '../../../shared/utils/errorResponse';
+import { parseStringArray } from '../../../shared/utils/utils';
 
 export default class OrgUserService {
   private orgUsersDao = new OrgUsersDao();
+
   async create({ org_id, user_id }: ICreateOrgUser): Promise<IOrgUser> {
+    const org_user = await this.orgUsersDao.selectByOrgUser(org_id, user_id);
+    if (org_user) throw new ErrorResponse(400, 'User Already Exist in Specified Organization!');
+
     const data = await this.orgUsersDao.create({ org_id, user_id });
+    if (!data) throw new ErrorResponse(500, 'Failed to Create!');
+    return data;
+  }
+
+  async createMany(org_id, user_ids: string[]): Promise<IOrgUser[]> {
+    const parsedData = parseStringArray(user_ids);
+
+    const insertData = parsedData.map((user_id) => {
+      return { org_id, user_id };
+    });
+
+    const data = await this.orgUsersDao.createMany(insertData);
+
     if (!data) throw new ErrorResponse(404, 'Failed to Create!');
     return data;
   }
+
   async getAll(): Promise<IOrgUser[]> {
     return await this.orgUsersDao.selectAll();
   }
