1c9901649b662fdf04ac1111249fdea68d953ed6
Farruh Olimov <farruhbekolimov3904@gmail.com>
2023-05-10 17:14:38 +0500

    add [projects, tasks, stats] routers

--- src/server/router.ts
+++ src/server/router.ts
@@ -3,15 +3,24 @@ import { Router } from 'express';
 import UsersRoute from '../modules/users/users.route';
 import RolesRoute from '../modules/roles/roles.route';
 import OrgsRoute from '../modules/organizations/organizations.route';
+import ProjectsRoute from '../modules/projects/projects.route';
+import TasksRoute from '../modules/tasks/tasks.route';
+import StatsRoute from '../modules/stats/stats.route';
 
 const router = Router();
 
 const usersRoute = new UsersRoute();
 const rolesRoute = new RolesRoute();
 const orgsRoute = new OrgsRoute();
+const projectsRoute = new ProjectsRoute();
+const tasksRoute = new TasksRoute();
+const statsRoute = new StatsRoute();
 
 router.use('/', usersRoute.router);
 router.use('/', rolesRoute.router);
 router.use('/', orgsRoute.router);
+router.use('/', projectsRoute.router);
+router.use('/', tasksRoute.router);
+router.use('/', statsRoute.router);
 
 export default router;
